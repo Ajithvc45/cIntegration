a9d2e6365c6d2cbdb71b8288f51bcf013a00dd4e
Ajithvc45 <ajithvc45@gmail.com>
2022-10-29 11:35:47 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  enterName = "Ajith";
+  parentData = "";
+  value = "";
+
   title = 'cIntegration';
+
+  TransferData() {
+    this.parentData = this.enterName;
+  }
+
+  sendData(value:any) {
+    this.value = value;
+  }
+
 }
